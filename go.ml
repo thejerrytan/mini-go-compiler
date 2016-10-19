792aa1d1054c63045d97d0ba9adac09e25de6635
Andhieka Putra <andhieka.putra@gmail.com>
2016-10-20 00:01:09 +0800

    Change unnecessarily lengthy name

--- go.ml
+++ go.ml
@@ -41,61 +41,61 @@ let print_string d s = (indent d) ^ s
 
 let linify xs = String.concat "\n" xs
 
-let rec pretty_print_exp d s = match s with
-  | And(x,y) -> linify [(indent d) ^ "And"; pretty_print_exp (d + 1) x; pretty_print_exp (d + 1) y]
-  | Eq(x,y) -> linify [(indent d) ^ "Eq"; pretty_print_exp (d + 1) x; pretty_print_exp (d + 1) y]
-  | Gt(x,y) -> linify [(indent d) ^ "Gt"; pretty_print_exp (d + 1) x; pretty_print_exp (d + 1) y]
-  | Plus(x,y) -> linify [(indent d) ^ "Plus"; pretty_print_exp (d + 1) x; pretty_print_exp (d + 1) y]
-  | Minus(x,y) -> linify [(indent d) ^ "Minus"; pretty_print_exp (d + 1) x; pretty_print_exp (d + 1) y]
-  | Times(x,y) -> linify [(indent d) ^ "Times"; pretty_print_exp (d + 1) x; pretty_print_exp (d + 1) y]
-  | Division(x,y) -> linify [(indent d) ^ "Division"; pretty_print_exp (d + 1) x; pretty_print_exp (d + 1) y]
-  | Not(x) -> linify [(indent d) ^ "Not"; pretty_print_exp (d + 1) x]
+let rec print_exp d s = match s with
+  | And(x,y) -> linify [(indent d) ^ "And"; print_exp (d + 1) x; print_exp (d + 1) y]
+  | Eq(x,y) -> linify [(indent d) ^ "Eq"; print_exp (d + 1) x; print_exp (d + 1) y]
+  | Gt(x,y) -> linify [(indent d) ^ "Gt"; print_exp (d + 1) x; print_exp (d + 1) y]
+  | Plus(x,y) -> linify [(indent d) ^ "Plus"; print_exp (d + 1) x; print_exp (d + 1) y]
+  | Minus(x,y) -> linify [(indent d) ^ "Minus"; print_exp (d + 1) x; print_exp (d + 1) y]
+  | Times(x,y) -> linify [(indent d) ^ "Times"; print_exp (d + 1) x; print_exp (d + 1) y]
+  | Division(x,y) -> linify [(indent d) ^ "Division"; print_exp (d + 1) x; print_exp (d + 1) y]
+  | Not(x) -> linify [(indent d) ^ "Not"; print_exp (d + 1) x]
   | RcvExp(x) -> linify [(indent d) ^ "RcvExp"; print_string (d + 1) x]
   | IConst(x) -> linify [(indent d) ^ "IConst"; print_string (d + 1) (string_of_int x)]
   | BConst(x) -> linify [(indent d) ^ "BConst"; print_string (d + 1) (string_of_bool x)]
   | Var(x) -> linify [(indent d) ^ "Var"; print_string (d + 1) x]
-  | FuncExp(x, ys) -> linify [(indent d) ^ "FuncExp"; print_string (d + 1) x; pretty_print_exp_list (d + 1) ys]
-and pretty_print_exp_list d s = 
-  linify (((indent d) ^ "List") :: (List.map (pretty_print_exp (d + 1)) s))
+  | FuncExp(x, ys) -> linify [(indent d) ^ "FuncExp"; print_string (d + 1) x; print_exp_list (d + 1) ys]
+and print_exp_list d s = 
+  linify (((indent d) ^ "List") :: (List.map (print_exp (d + 1)) s))
 
-let rec pretty_print_type d s = match s with
+let rec print_type d s = match s with
   | TyInt   -> (indent d) ^ "TyInt"
   | TyBool  -> (indent d) ^ "TyBool"
-  | TyChan t -> linify [(indent d) ^ "TyChan"; pretty_print_type (d + 1) t;]
-  | TyFunc (ts, t) -> linify [(indent d) ^ "TyFunc"; pretty_print_type_list (d + 1) ts; pretty_print_type (d + 1) t]
-and pretty_print_type_list d s = 
-  linify (((indent d) ^ "List") :: (List.map (pretty_print_type (d + 1)) s))
+  | TyChan t -> linify [(indent d) ^ "TyChan"; print_type (d + 1) t;]
+  | TyFunc (ts, t) -> linify [(indent d) ^ "TyFunc"; print_type_list (d + 1) ts; print_type (d + 1) t]
+and print_type_list d s = 
+  linify (((indent d) ^ "List") :: (List.map (print_type (d + 1)) s))
 
 let print_type_option d s = match s with
-  | Some(x)-> linify [(indent d) ^ "Some"; pretty_print_type (d + 1) x]
+  | Some(x)-> linify [(indent d) ^ "Some"; print_type (d + 1) x]
   | None -> (indent d) ^ "None"
 
-let pretty_print_exp_type d s =
-  linify [(indent d) ^ "Tuple"; pretty_print_exp (d + 1) (fst s); pretty_print_type (d + 1) (snd s)]
+let print_exp_type d s =
+  linify [(indent d) ^ "Tuple"; print_exp (d + 1) (fst s); print_type (d + 1) (snd s)]
 
-let pretty_print_exp_type_list d s =
-  linify (((indent d) ^ "List") :: (List.map (pretty_print_exp_type (d + 1)) s))
+let print_exp_type_list d s =
+  linify (((indent d) ^ "List") :: (List.map (print_exp_type (d + 1)) s))
 
-let rec pretty_print_stmt d s = match s with
-  | Seq(x,y) -> linify [(indent d) ^ "Seq"; pretty_print_stmt (d + 1) x; pretty_print_stmt (d + 1) y]
-  | Go(x)    -> linify [(indent d) ^ "Go"; pretty_print_stmt (d + 1) x]
-  | Transmit(x, y) -> linify [(indent d) ^ "Transmit"; print_string (d + 1) x; pretty_print_exp (d + 1) y]
+let rec print_stmt d s = match s with
+  | Seq(x,y) -> linify [(indent d) ^ "Seq"; print_stmt (d + 1) x; print_stmt (d + 1) y]
+  | Go(x)    -> linify [(indent d) ^ "Go"; print_stmt (d + 1) x]
+  | Transmit(x, y) -> linify [(indent d) ^ "Transmit"; print_string (d + 1) x; print_exp (d + 1) y]
   | RcvStmt(x) -> linify [(indent d) ^ "RcvStmt"; print_string (d + 1) x]
-  | Decl(x,y) -> linify [(indent d) ^ "Decl"; print_string (d + 1) x; pretty_print_exp (d + 1) y]
+  | Decl(x,y) -> linify [(indent d) ^ "Decl"; print_string (d + 1) x; print_exp (d + 1) y]
   | DeclChan(x) -> linify [(indent d) ^ "DeclChan"; print_string (d + 1) x]
-  | Assign(x,y) -> linify [(indent d) ^ "Assign"; print_string (d + 1) x; pretty_print_exp (d + 1) y]
-  | While(x, y) -> linify [(indent d) ^ "While"; pretty_print_exp (d + 1) x; pretty_print_stmt (d + 1) y]
-  | ITE(x,y,z) -> linify ["ITE"; pretty_print_exp (d + 1) x; pretty_print_stmt (d + 1) y; pretty_print_stmt (d + 1) z]
-  | Return(x) -> linify ["Return"; pretty_print_exp (d + 1) x]
-  | FuncCall(x, ys) -> linify [(indent d) ^ "FuncCall"; print_string (d + 1) x; pretty_print_exp_list (d + 1) ys]
-  | Print(x) -> linify [(indent d) ^ "Print"; pretty_print_exp (d + 1) x]
+  | Assign(x,y) -> linify [(indent d) ^ "Assign"; print_string (d + 1) x; print_exp (d + 1) y]
+  | While(x, y) -> linify [(indent d) ^ "While"; print_exp (d + 1) x; print_stmt (d + 1) y]
+  | ITE(x,y,z) -> linify ["ITE"; print_exp (d + 1) x; print_stmt (d + 1) y; print_stmt (d + 1) z]
+  | Return(x) -> linify ["Return"; print_exp (d + 1) x]
+  | FuncCall(x, ys) -> linify [(indent d) ^ "FuncCall"; print_string (d + 1) x; print_exp_list (d + 1) ys]
+  | Print(x) -> linify [(indent d) ^ "Print"; print_exp (d + 1) x]
   | Skip -> (indent d) ^ "Skip"
 
-let pretty_print_proc d s = match s with
-  | Proc(x, ys, z, s) -> linify [(indent d) ^ "Proc"; print_string (d + 1) x; pretty_print_exp_type_list (d + 1) ys; print_type_option (d + 1) z; pretty_print_stmt (d + 1) s]
+let print_proc d s = match s with
+  | Proc(x, ys, z, s) -> linify [(indent d) ^ "Proc"; print_string (d + 1) x; print_exp_type_list (d + 1) ys; print_type_option (d + 1) z; print_stmt (d + 1) s]
 
-let pretty_print_proc_list d s = 
-  linify (((indent d) ^ "List") :: (List.map (pretty_print_proc (d + 1)) s))
+let print_proc_list d s = 
+  linify (((indent d) ^ "List") :: (List.map (print_proc (d + 1)) s))
 
-let rec pretty_print_prog d s = match s with
-  | Prog(xs, y) -> linify [(indent d) ^ "Prog"; pretty_print_proc_list (d + 1) xs; pretty_print_stmt (d + 1) y]
+let rec print_prog d s = match s with
+  | Prog(xs, y) -> linify [(indent d) ^ "Prog"; print_proc_list (d + 1) xs; print_stmt (d + 1) y]
