4e8545373c8248651b1d551594216f2b567b8a54
Andhieka Putra <andhieka.putra@gmail.com>
2016-11-13 08:46:41 +0800

    Un-ignore lexerl.mll and fix the order of build in run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 ocamllex lexer.mll       # generates lexer.ml
 ocamlyacc parser.mly     # generates parser.ml and parser.mli
 ocamlc -c -g go.ml
-ocamlc -c -g lexer.ml
 ocamlc -c -g parser.mli
 ocamlc -c -g parser.ml
+ocamlc -c -g lexer.ml
 # ocamlc -c -g ty.ml
 ocamlc -c -g normalize.ml
 ocamlc -c -g intermediate.ml
