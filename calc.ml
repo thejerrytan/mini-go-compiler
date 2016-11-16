9d9cfd780cab8e2adff4cd5ee684c3c163d22150
Jerry Tan <jerrytansk@gmail.com>
2016-11-16 16:50:30 +0800

    parser forces proc with return type to end with a return stmt

--- calc.ml
+++ calc.ml
@@ -1,7 +1,7 @@
 (* File calc.ml *)
 open Go
 (* open Intermediate *)
-open Ty
+(* open Ty *)
 (* open Vm *)
 (* open Normalize *)
 
@@ -40,11 +40,11 @@ let compiler src = match (parser src) with
  *)
 (* Testing *)
 let parserTests = [
-	(* "./tests/ex1.go"; *)
-	(* "./tests/ex2.go"; *)
-	(* "./tests/ex3.go" *)
+	"./tests/ex1.go";
+	"./tests/ex2.go";
+	"./tests/ex3.go"
   (* "./tests/normalizeTest.go" *)
-  "./tests/typeCheckerDeclarationsTest.go"
+  (* "./tests/typeCheckerDeclarationsTest.go" *)
   (* "./tests/typeCheckerFunctionTest.go" *)
   (* "./tests/typeCheckerFunctionFailureTest.go" *)
 ]
@@ -83,18 +83,18 @@ let testParser =
   let parserAstList = List.map parseAst parserTests in
   	List.map printAst parserAstList
 
-let testAst (src, ast) = match typeCheckProg [] ast with
+(* let testAst (src, ast) = match typeCheckProg [] ast with
                          | Some ast -> Printf.printf "%s" (print_prog 0 ast); print_newline(); flush stdout;
                          | None -> Printf.printf "%s" "fail"; print_newline(); flush stdout
-
-let testTypeChecker =
+ *)
+(* let testTypeChecker =
   let parserAstList = List.map parseAst typeCheckerTests in
     List.map testAst parserAstList
-
+ *)
 (* let testNormParser =
   let parserNormAstList = List.map parseNormAst parserTests in
     List.map printNormAst parserNormAstList *)
 
-let _ = testTypeChecker
-(* let _ = testParser *)
+(* let _ = testTypeChecker *)
+let _ = testParser
 (* let _ = testNormParser  *)
